cd5a9a85fc4891c2f193f06be6fdd5b7689653d6
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-20 18:39:46 -0300

    Criando end-point POST Produto

--- src/main/java/com/carloshsrosa/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/carloshsrosa/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,29 @@
 package com.carloshsrosa.produtosapi.controller;
 
 import com.carloshsrosa.produtosapi.model.Produto;
+import com.carloshsrosa.produtosapi.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    @Autowired
+    ProdutoRepository repository;
+
     @PostMapping
-    public Produto postProduto(@RequestBody Produto produto){
-        return produto;
+    public ResponseEntity<Void> gravaProduto(@RequestBody Produto produto){
+        repository.save(produto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(produto.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
