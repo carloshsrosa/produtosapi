c46366b5ebc9e7d12969ca8ce358110cb7718030
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-20 18:44:10 -0300

    Criando end-point GET Produto

--- src/main/java/com/carloshsrosa/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/carloshsrosa/produtosapi/controller/ProdutoController.java
@@ -4,13 +4,11 @@ import com.carloshsrosa.produtosapi.model.Produto;
 import com.carloshsrosa.produtosapi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("produtos")
@@ -19,11 +17,16 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository repository;
 
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAllProduto(){
+        List<Produto> produtos = repository.findAll();
+        return ResponseEntity.ok().body(produtos);
+    }
+
     @PostMapping
-    public ResponseEntity<Void> gravaProduto(@RequestBody Produto produto){
+    public ResponseEntity<Void> saveProduto(@RequestBody Produto produto){
         repository.save(produto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(produto.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
-
 }
