e0ca2784a73cd730ce9f32b72695164b0b44ea57
Carlos Viegas <carloshsrosa@gmail.com>
2025-02-21 10:19:35 -0300

    Criando end-point GET Produto por ID e DELETE Produto

--- src/main/java/com/carloshsrosa/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/carloshsrosa/produtosapi/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package com.carloshsrosa.produtosapi.controller;
 
 import com.carloshsrosa.produtosapi.model.Produto;
 import com.carloshsrosa.produtosapi.repository.ProdutoRepository;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +19,35 @@ public class ProdutoController {
     ProdutoRepository repository;
 
     @GetMapping
-    public ResponseEntity<List<Produto>> findAllProduto(){
+    public ResponseEntity<List<Produto>> findAll(){
         List<Produto> produtos = repository.findAll();
         return ResponseEntity.ok().body(produtos);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
+        Produto produto = repository.findById(id).orElse(null);
+        if (produto == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(produto);
+    }
+
     @PostMapping
-    public ResponseEntity<Void> saveProduto(@RequestBody Produto produto){
+    public ResponseEntity<Void> save(@RequestBody Produto produto){
         repository.save(produto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(produto.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        Produto produto = repository.findById(id).orElse(null);
+        if (produto == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            repository.delete(produto);
+        }
+        return ResponseEntity.noContent().build();
     }
 }
